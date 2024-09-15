6a1a5de32307bd25eb1326a96c181a5a6c5b8d95
HilsSensi <hillalnurazhar@gmail.com>
2024-09-15 09:56:15 +0700

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -40,7 +40,27 @@ echo
     wm size reset 
     wm density reset
     settings put secure multi_press_timeout 400
-    settings put secure long_press_timeout 400
+    settings put secure long_press_timeout 400 
+    settings delete put global force_gpu_rendering 1
+settings delete put system touch_sensitivity 1 enabled_hardware_optimization 1
+settings delete put system enabled_ui_optimization 1
+settings delete put system status_bar_animations_duration_scale 0.5
+settings delete put system navigation_bar_animations_duration_scale 0.5
+settings delete put system power_menu_expanded_ 130
+settings delete put system touch_deley 0.1
+settings delete put system tap_timeout 100 
+settings delete put system long_press_timeout 300  
+settings delete put system gpu_render_policy 1
+settings delete put system bg_apps_limit 4
+settings delete put system hardware_rendering_enabled 1
+settings delete put system notification_slide_animations_duration 0.30
+settings delete put system app_close_animated 0.15
+settings delete put global window_animation_scale 0.5
+settings delete put global transition_animation_scale 0.5
+settings delete put global animator_duration_scale 0.5
+
+settings put system pointer_speed 1
+settings delete put system touchpanel_edge_filter 0
     rm $sizeath
     rm $pathking
     rm $pathfozx
