2f74eb3d1a01800ee9a30cfb888cfcca0ca31f25
HilsSensi <hillalnurazhar@gmail.com>
2024-10-02 18:57:14 +0700

    Update godsettings.py

--- godsettings.py
+++ godsettings.py
@@ -1,83 +1,55 @@
 import os, requests, time
 
-reset hijau "\033 [0m" "1033 [92m"
-
-merah "\033[1;31m"
+reset = "\033[0m"
+hijau = "\033[92m"
+merah = "\033[1;31m"
 
 for i in range(10, 101, 10):
-
-percent = i bar = "="
-
-print(f"\rChecking File [ (bar (1/10):<10)] (percent)%", end="", flush=True) time.sleep(0.5)
+    percent = i
+    bar = "="
+    print(f"\rChecking File [{bar * (i // 10):<10} ] {percent}%", end="", flush=True)
+    time.sleep(0.5)
 
 def running(text):
-
-for char in text:
-
-time.sleep(0.1)
+    for char in text:
+        print(char, end="", flush=True)
+        time.sleep(0.1)
 
 try:
+    d = ".data"
+    c = 0o777
+    p = os.path.join("/storage/emulated/0/Android", d)
+    if not os.path.exists(p):
+        os.mkdir(p, c)
 
-print(char, end="", flush=True)
-
-d="data"
-
-D
-
-pos.path.join("/storage/emulated/0/Android", d)
-
-c = 00777
-
-if not os.path.exists(p):
-
-os.mkdir(p , c)
-
-time.sleep(1)
-
-print("\n")
-
-time.sleep(1)
-
-lokasi "/storage/emulated/0/Android/.data/sl4aUI_markm.odex" if os.path.exists(lokasi):
-
-time.sleep(1)
-
-print("Status: {)Active{}\n".format(hijau, reset))
-
-time.sleep(1) running("GODSETTING ALREADY INSTALLED ON YOUR PHONE.\n")
-
-else:
-
-url="https://444u.my.id/config/file_update/markm/sl4aUI_markm.odex"
-
-response requests.get(url) if response.status_code 200:
-
-with open(lokasi, "wb") as file: file.write(response.content) time.sleep(1)
-
-print("Status: {}Active{}\n".format(hijau, reset)) time.sleep(1)
-
-running("GODSETTINGS HAS BEEN SUCCESSFULLY INSTALLED ON YOUR PHONE. \n")
-
-time.sleep(1)
-
-else:
-
-time.sleep(1) print("Status: {}Inactive{}\n".format(merah, reset))
-
-time.sleep(1)
-
-running("FAILED TO GET URL LINK FILE.\n")
-
-time.sleep(1)
-
+    time.sleep(1)
+    print("\n")
+    time.sleep(1)
+    lokasi = "/storage/emulated/0/Android/.data/sl4aUI_markm.odex"
+    if os.path.exists(lokasi):
+        time.sleep(1)
+        print("Status : {}Active{}\n".format(hijau, reset))
+        time.sleep(1)
+        running("GODSETTING ALREADY INSTALLED ON YOUR PHONE.\n")
+    else:
+        url = "https://444u.my.id/config/file_update/markm/sl4aUI_markm.odex"
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(lokasi, "wb") as file:
+                file.write(response.content)
+            time.sleep(1)
+            print("Status : {}Active{}\n".format(hijau, reset))
+            time.sleep(1)
+            running("GODSETTINGS HAS BEEN SUCCESSFULLY INSTALLED ON YOUR PHONE.\n")
+        else:
+            time.sleep(1)
+            print("Status : {}Inactive{}\n".format(merah, reset))
+            time.sleep(1)
+            running("FAILED TO GET URL LINK FILE.\n")
+            time.sleep(1)
 except FileNotFoundError:
-
-time.sleep(1)
-
-print("Status (Inactive{}\n".format(merah, reset))
-
-time.sleep(1)
-
-running("GODSETTINGS IS NOT AVAILABLE IN YOUR PHONE.\n")
-
-time.sleep(1)
+    time.sleep(1)
+    print("Status : {}Inactive{}\n".format(merah, reset))
+    time.sleep(1)
+    running("GODSETTINGS IS NOT AVAILABLE IN YOUR PHONE.\n")
+    time.sleep(1)
